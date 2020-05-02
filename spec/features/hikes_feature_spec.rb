69817681e428eca3200aa9a2d38891e1bd1e279c
Mitchel <mwassler@covermymeds.com>
2020-05-02 14:15:49 -0400

    fix feature test

--- spec/features/hikes_feature_spec.rb
+++ spec/features/hikes_feature_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "HikesFeatures", type: :feature do
+  let :user do
+    User.create! username:              'tester',
+                 email:                 'test@user.com',
+                 password:              'secret',
+                 password_confirmation: 'secret'
+  end
+
   scenario 'User uploads a new GPX file' do
+    sign_in user
     visit '/hikes/new'
-    attach_file 'hike[path_file]', file_fixture('sample.gpx')
+    attach_file 'hike[path_file]', file_fixture('sample.gpx'), make_visible: true
     click_button 'Create Hike'
     expect(page).to have_text 'Hike was successfully created.'
   end
